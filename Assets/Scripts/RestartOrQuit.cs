349c6f94e6374fcc61739e164b00129d656d8633
Antonina Ivanova <a.ivanova.lviv@gmail.com>
2020-06-19 00:13:21 +0300

    First commit

---
+++ Assets/Scripts/RestartOrQuit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartOrQuit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //// Update is called once per frame
+    //public void ChangeToScene(string LoadLevel)
+    //{
+    //    Application.LoadLevel(0);
+    //}
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
